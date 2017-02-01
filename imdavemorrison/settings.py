019df6035c016adc23ffdc9bb57f98c13cf2e5a9
Dave <davem220@gmail.com>
2017-01-31 20:27:12 -0500

    changed some styles and other things in my home template

--- imdavemorrison/settings.py
+++ imdavemorrison/settings.py
@@ -102,7 +102,7 @@ EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'ensensefinance@gmail.com'
-EMAIL_HOST_PASSWORD = 'H2*Yzm7SL'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 
 
